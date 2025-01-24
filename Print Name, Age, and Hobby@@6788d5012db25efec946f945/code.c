c596d584e91f750a3d37fc86bb6f88de9886e40d
sonam-24-git <sonam0791.becse24@chitkara.edu.in>
2025-01-24 12:36:36 +0530

    Solution to Print Name, Age, and Hobby

---
+++ Print Name, Age, and Hobby@@6788d5012db25efec946f945/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char a[30];
+    char b[30];
+    char c[30];
+    scanf("%s",&a);
+    scanf("%s",&b);
+    scanf("%s",&c);
+    
+    printf("%s", welcome());
+    return 0;
+}
