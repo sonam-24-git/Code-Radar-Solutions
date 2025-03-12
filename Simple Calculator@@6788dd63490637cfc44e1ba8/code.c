c90c4c477aa125046492efb797df333179e4e6f6
Sonam00 <sonam0791.becse24@chitkara.edu.in>
2025-03-12 22:42:24 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    int add,sub,mul,div;
+    if(add=+){
+        printf("%d",a+b);
+    }
+}
