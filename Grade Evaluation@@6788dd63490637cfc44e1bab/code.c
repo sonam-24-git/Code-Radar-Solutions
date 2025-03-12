afb3fad733e69f8886661ed3cda9b1e196e0a38b
Sonam00 <sonam0791.becse24@chitkara.edu.in>
2025-03-12 23:51:25 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    char c;
+    scanf(" %c",%c);
+    if(c=='A'){
+        printf("Excellent");
+    }else if(c=='B'){
+        printf("Good");
+    }else if(c=='C'){
+        printf("Average");
+    }else if(c=='D'){
+        printf("Below Average");
+    }else if(c=='E'){
+        printf("Fail")
+    }else{
+        printf("Invalid grade");
+    }
+    return 0;
+}
